1f3cad4291ea6387aba48bc6d3b0bc9f3787981b
Cường Trần <41182580+MinhCuongIT@users.noreply.github.com>
2020-12-15 08:05:35 +0700

    change update method

--- demo_trans/lib/main.dart
+++ demo_trans/lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:translator/translator.dart';
 
-
-void main()
-{
-  runApp(
-    MaterialApp(
-      home: App(),
-    )
-  );
-  
+void main() {
+  runApp(MaterialApp(
+    home: App(),
+  ));
 }
 
 class App extends StatefulWidget {
@@ -18,54 +13,53 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  GoogleTranslator translator = new GoogleTranslator();
 
-  GoogleTranslator translator = new GoogleTranslator();   //using google translator
-  
-  
   String out;
-  final lang=TextEditingController();   //getting text
+  final lang = TextEditingController();
 
-
-  void trans()
-  {
-    
-    translator.translate(lang.text, to: 'hi')   //translating to hi = hindi
-      .then((output) 
-      {
-          setState(() {
-           out=output;                          //placing the translated text to the String to be used
-          });
-          print(out);
+  void trans() {
+    translator.translate(lang.text, to: 'vi').then((output) {
+      setState(() {
+        out =
+            output.text; //placing the translated text to the String to be used
       });
+      print(out);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Transalate !!"),
+        title: Text("Translate"),
       ),
       body: Container(
         child: Center(
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: lang,
+            child: Column(
+          children: <Widget>[
+            TextField(
+              controller: lang,
+            ),
+            RaisedButton(
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
               ),
-              RaisedButton(
-            color: Colors.red,
-            child: Text("Press !!"),            //on press to translate the language using function
-            onPressed: ()
-            {
-              trans();
-            },
-          ),
-          Text(out.toString())                    //translated string
-            ],
-          )
-        ),
+              child: Text(
+                "Translate now",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                trans();
+              },
+            ),
+            Text(out.toString()) //translated string
+          ],
+        )),
       ),
     );
   }
 }
-
